bf5b5409ac53791e9309ad109e4198c700105411
mianazeemdaula <mianazeemdaula@gmail.com>
2022-08-12 21:08:56 +0500

    casts

--- app/Models/SellerProfile.php
+++ app/Models/SellerProfile.php
@@ -8,22 +8,28 @@ use Illuminate\Database\Eloquent\Model;
 class SellerProfile extends Model
 {
     use HasFactory;
-    protected $fillable = [
-        'user_id',
-        'profile_image',
-        'business_name',
-        'business_email',
-        'phone_no',
-        'website_url',
-        'address',
-        'zipcode',
-        'country',
-        'city',
-        'is_license',
-        'is_home_based',
-        'is_bonded',
-        'is_insured',
-
-
+    protected $casts = [
+        'is_license' => 'boolean',
+        'is_home_based' => 'boolean',
+        'is_bonded' => 'boolean',
+        'is_insured' => 'boolean',
     ];
+    // protected $fillable = [
+    //     'user_id',
+    //     'profile_image',
+    //     'business_name',
+    //     'business_email',
+    //     'phone_no',
+    //     'website_url',
+    //     'address',
+    //     'zipcode',
+    //     'country',
+    //     'city',
+    //     'is_license',
+    //     'is_home_based',
+    //     'is_bonded',
+    //     'is_insured',
+
+
+    // ];
 }
